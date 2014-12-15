284ab93324e293e25d91f8791a188554c20e526e
John Hoffman <johnh@qualtrics.com>
2014-12-15 14:43:37 -0700

    RepeatParser: updated expression format to work with ui-scroll and removed group feature

--- src/select.js
+++ src/select.js
@@ -118,39 +118,29 @@
 
     /**
      * Example:
-     * expression = "address in addresses | filter: {street: $select.search} track by $index"
+     * expression = "address in addresses | filter: {street: $select.search}"
      * itemName = "address",
      * source = "addresses | filter: {street: $select.search}",
-     * trackByExp = "$index",
      */
     self.parse = function(expression) {
 
-      var match = expression.match(/^\s*(?:([\s\S]+?)\s+as\s+)?([\S]+?)\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?\s*$/);
+      var match = expression.match(/^\s*([\S]+?)\s+in\s+([\s\S]+?)\s*$/);
 
       if (!match) {
-        throw uiSelectMinErr('iexp', "Expected expression in form of '_item_ in _collection_[ track by _id_]' but got '{0}'.",
+        throw uiSelectMinErr('iexp', "Expected expression in form of '_item_ in _collection_' but got '{0}'.",
                 expression);
       }
 
       return {
-        itemName: match[2], // (lhs) Left-hand side,
-        source: $parse(match[3]),
-        trackByExp: match[4],
-        modelMapper: $parse(match[1] || match[2])
+        itemName: match[1], // (lhs) Left-hand side,
+        source: $parse(match[2]),
+        modelMapper: $parse(match[1])
       };
 
     };
 
-    self.getGroupNgRepeatExpression = function() {
-      return '$group in $select.groups';
-    };
-
-    self.getNgRepeatExpression = function(itemName, source, trackByExp, grouped) {
-      var expression = itemName + ' in ' + (grouped ? '$group.items' : source);
-      if (trackByExp) {
-        expression += ' track by ' + trackByExp;
-      }
-      return expression;
+    self.getUiScrollExpression = function(itemName, source) {
+      return itemName + ' in ' + source;
     };
   }])
 
